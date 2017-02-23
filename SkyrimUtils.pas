5d06547326519242362a78ce637a4e1224db8318
AngryAndConflict <arkadip13@gmail.com>
2017-02-23 23:17:10 +0300

    Bulletproofing getRecordByFormID
    basic rules now:
    1. consider not found, unless it is
    2. firstly check if enough plugins are loaded, to have record by load order
    3. check that plugin was found
    4. only now check record to be found

--- SkyrimUtils.pas
+++ SkyrimUtils.pas
@@ -156,23 +156,24 @@ function getRecordByFormID(id: string): IInterface;
 var
   tmp: IInterface;
 begin
-  // basically we took record like 00049BB7, and by slicing 2 first symbols, we get its file index, in this case Skyrim (00)
-  tmp := FileByLoadOrder(StrToInt('$' + Copy(id, 1, 2)));
+  // if file or record was not found => return nil
+  Result := nil;
 
-  // file was found
-  if Assigned(tmp) then begin
-    // look for this record in founded file, and return it
-    tmp := RecordByFormID(tmp, StrToInt('$' + id), true);
+  // if records plugin id is loaded
+  if not (StrToInt(Copy(id, 1, 2)) > (FileCount - 1)) then begin
+    // basically we took record like 00049BB7, and by slicing 2 first symbols, we get its file index, in this case Skyrim (00)
+    tmp := FileByLoadOrder(StrToInt('$' + Copy(id, 1, 2)));
 
-    // check that record was found
+    // file was found
     if Assigned(tmp) then begin
-      Result := tmp;
-    end else begin // return nil if not
-      Result := nil;
-    end;
+      // look for this record in founded file, and return it
+      tmp := RecordByFormID(tmp, StrToInt('$' + id), true);
 
-  end else begin // return nil if not
-    Result := nil;
+      // check that record was found
+      if Assigned(tmp) then
+        Result := tmp;
+
+    end;
   end;
 end;
 
