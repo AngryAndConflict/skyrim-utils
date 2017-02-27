9cb9f58156d2655270bc9d907eb77cb9107e604f
AngryAndConflict <AngryAndConflict@users.noreply.github.com>
2017-02-27 11:00:15 +0300

    Check editorID

--- Skyrim - Linter.pas
+++ Skyrim - Linter.pas
@@ -20,6 +20,8 @@ const
 
   skipRecords = 'NAVM ACHR PGRE PHZD';
 
+	EDITOR_ID_MIGHT_BE_REQUIRED = 'WEAP ARMO AMMO BOOK MISC LVLI CELL NPC_';
+
 procedure lint(recordToCheck: IInterface);
 var
 	itemSignature: string;
@@ -42,7 +44,18 @@ procedure lintStrings(recordToCheck: IInterface; itemType: string);
 var
 	tmp: string;
 begin
-	
+	if (Pos(itemType, EDITOR_ID_MIGHT_BE_REQUIRED) <> 0) then begin
+		tmp := GetElementEditValues(recordToCheck, 'EDID');
+
+		if not Assigned(tmp) then begin
+			AddMessage(msgReallyBad + ' EditorID is missing ' + msgHr + ' ' + Name(recordToCheck));
+		end else if ((tmp = '') or (Length(tmp) = 0)) then begin
+			AddMessage(msgReallyBad + ' EditorID is empty string ' + msgHr + ' ' + Name(recordToCheck));
+		end else if (Length(tmp) < 5) then begin
+			AddMessage(msgBad + ' EditorID is too short to not become duplicate ' + msgHr + ' ' + Name(recordToCheck));
+		end;
+	end;
+
 end;
 
 function Process(selectedRecord: IInterface): integer;
