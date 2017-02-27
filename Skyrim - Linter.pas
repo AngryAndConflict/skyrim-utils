eb9b679c946450e9137a065437c04d7d9bb25186
AngryAndConflict <AngryAndConflict@users.noreply.github.com>
2017-02-27 11:12:19 +0300

    Check FULL string

--- Skyrim - Linter.pas
+++ Skyrim - Linter.pas
@@ -12,15 +12,16 @@ const
 	// =Settings
 	tryToCorrect = false;
 
-  msgHr = '---------->';
-  msgReallyBad = '---BAD!---';
-  msgBad = '---Silent erorr---';
-  msgNote = '--notice: ';
-  msgCorrection = '-FIXING ATTEMPT--';
+	msgHr = '---------->';
+	msgReallyBad = '---BAD!---';
+	msgBad = '---Silent erorr---';
+	msgNote = '--notice: ';
+	msgCorrection = '-FIXING ATTEMPT--';
 
-  skipRecords = 'NAVM ACHR PGRE PHZD';
+	skipRecords = 'NAVM ACHR PGRE PHZD';
 
 	EDITOR_ID_MIGHT_BE_REQUIRED = 'WEAP ARMO AMMO BOOK MISC LVLI CELL NPC_';
+	FULL_NAME_MAY_BE_REQUIRED = 'WEAP ARMO AMMO BOOK MISC NPC_';
 
 procedure lint(recordToCheck: IInterface);
 var
@@ -33,7 +34,7 @@ begin
 
 	recordSignature := Signature(recordToCheck);
 
-  // linter ignoring records
+	// linter ignoring records
 	if (Pos(recordSignature, skipRecords) <> 0) then
 		Exit;
 
@@ -56,11 +57,48 @@ begin
 		end;
 	end;
 
+	if (Pos(recordSignature, FULL_NAME_MAY_BE_REQUIRED) <> 0) then begin
+		tmp := GetElementEditValues(recordToCheck, 'FULL');
+
+		if not Assigned(tmp) then begin
+			AddMessage(msgNote + ' FULL Name is missing ' + msgHr + ' ' + Name(recordToCheck));
+
+		end else if ((tmp = '') or (Length(tmp) = 0)) then begin
+			AddMessage(msgNote + ' FULL Name is an empty string, it is recomanded to be deleted it if not using: ' + msgHr + ' ' + Name(recordToCheck));
+
+		end else if ((tmp[1] = ' ') or (tmp[Length(tmp)] = ' ')) then begin
+			AddMessage(msgBad + ' FULL NAME have trailing space, CK does not like that ' + msgHr + ' ' + Name(recordToCheck));
+			if tryToCorrect then begin
+				AddMessage(msgCorrection + ' removing trailing spaces: ' + msgHr + ' ' + Name(recordToCheck));
+				SetElementEditValues(recordToCheck, 'FULL', Trim(tmp));
+			end;
+		end;
+
+		tmp := GetElementEditValues(recordToCheck, 'FULL');
+		if Assigned(tmp) then begin
+			if (Length(tmp) > 33) then begin
+				AddMessage(msgBad + ' FULL NAME length is more than 33 characters, CK does not like that ' + msgHr + ' ' + Name(recordToCheck));
+			end;
+		end;
+
+		if (recordSignature = 'NPC_') then begin
+			tmp := GetElementEditValues(recordToCheck, 'FULL');
+
+			if Assigned(tmp) then begin
+				tmp := GetElementEditValues(recordToCheck, 'SHRT');
+				if not Assigned(tmp) then begin
+					AddMessage(msgNote + ' Short Name is missing for NPC, but FULL was provided ' + msgHr + ' ' + Name(recordToCheck));
+				end;
+			end;
+
+		end;
+	end;
+
 end;
 
 function Process(selectedRecord: IInterface): integer;
 begin
-  lint(selectedRecord);
+	lint(selectedRecord);
 end;
 
 function Finalize: integer;
