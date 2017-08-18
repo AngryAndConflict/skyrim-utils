b473a4b19cccaf3be328b0fdd28218cdbfea1519
AngryAndConflict <AngryAndConflict@Wenegred-iMac.local>
2017-08-18 21:06:33 +0300

    Minor fix
    Builder new line affection fix

--- build-utils.ps1
+++ build-utils.ps1
@@ -4,5 +4,6 @@ $utils += Get-Content utils-src\_globals.pas | Out-String
 $utils += "`n`n"
 
 $utils += Get-Content -Path utils-src\* -Exclude _notation.pas, _globals.pas | Out-String
+$utils += "`n"
 $utils += 'end.'
 Set-Content SkyrimUtils.pas $utils
