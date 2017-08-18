fec28f0e4afeb523175b507ca2cd0574c2288bb2
AngryAndConflict <AngryAndConflict@Wenegred-iMac.local>
2017-08-18 22:28:54 +0300

    Typo fixes

--- utils-src/makeTemperable.pas
+++ utils-src/makeTemperable.pas
@@ -1,67 +1,67 @@
 // creates new COBJ record to make item Temperable
 function makeTemperable(itemRecord: IInterface): IInterface;
 var
-  recipeTemper,
-    recipeCondition, recipeConditions,
-    recipeItem, recipeItems
-  : IInterface;
+	recipeTemper,
+		recipeCondition, recipeConditions,
+		recipeItem, recipeItems
+	: IInterface;
 begin
-  recipeTemper := createRecipe(itemRecord);
+	recipeTemper := createRecipe(itemRecord);
 
-  // add new condition list
-  Add(recipeTemper, 'Conditions', true);
-  // get reference to condition list inside recipe
-  recipeConditions := ElementByPath(recipeTemper, 'Conditions');
+	// add new condition list
+	Add(recipeTemper, 'Conditions', true);
+	// get reference to condition list inside recipe
+	recipeConditions := ElementByPath(recipeTemper, 'Conditions');
 
-  // add IsEnchanted condition
-  // get new condition from list
-  recipeCondition := ElementByIndex(recipeConditions, 0);
-  // set type to Not equal to / Or
-  SetElementEditValues(recipeCondition, 'CTDA - \Type', '00010000');
-  // set some needed properties
-  SetElementEditValues(recipeCondition, 'CTDA - \Comparison Value', '1');
-  SetElementEditValues(recipeCondition, 'CTDA - \Function', 'EPTemperingItemIsEnchanted');
-  SetElementEditValues(recipeCondition, 'CTDA - \Run On', 'Subject');
-  // don't know what is this, but it should be equal to -1, if Function Runs On Subject
-  SetElementEditValues(recipeCondition, 'CTDA - \Parameter #3', '-1');
+	// add IsEnchanted condition
+	// get new condition from list
+	recipeCondition := ElementByIndex(recipeConditions, 0);
+	// set type to 'Not equal to / Or'
+	SetElementEditValues(recipeCondition, 'CTDA - \Type', '00010000');
+	// set some needed properties
+	SetElementEditValues(recipeCondition, 'CTDA - \Comparison Value', '1');
+	SetElementEditValues(recipeCondition, 'CTDA - \Function', 'EPTemperingItemIsEnchanted');
+	SetElementEditValues(recipeCondition, 'CTDA - \Run On', 'Subject');
+	// don't know what is this, but it should be equal to -1, if Function Runs On Subject
+	SetElementEditValues(recipeCondition, 'CTDA - \Parameter #3', '-1');
 
-  // add second condition, for perk ArcaneBlacksmith check
-  addPerkCondition(recipeConditions, getRecordByFormID('0005218E')); // ArcaneBlacksmith
+	// add second condition, for perk ArcaneBlacksmith check
+	addPerkCondition(recipeConditions, getRecordByFormID('0005218E')); // ArcaneBlacksmith
 
-  // add required items list
-  Add(recipeTemper, 'items', true);
-  // get reference to required items list inside recipe
-  recipeItems := ElementByPath(recipeTemper, 'items');
+	// add required items list
+	Add(recipeTemper, 'items', true);
+	// get reference to required items list inside recipe
+	recipeItems := ElementByPath(recipeTemper, 'items');
 
-  if Signature(itemRecord) = 'WEAP' then begin
-    // set EditorID for recipe
-    SetElementEditValues(recipeTemper, 'EDID', 'TemperWeapon' + GetElementEditValues(itemRecord, 'EDID'));
+	if Signature(itemRecord) = 'WEAP' then begin
+		// set EditorID for recipe
+		SetElementEditValues(recipeTemper, 'EDID', 'TemperWeapon' + GetElementEditValues(itemRecord, 'EDID'));
 
-    // add reference to the workbench keyword
-    SetElementEditValues(recipeTemper, 'BNAM', GetEditValue(
-      getRecordByFormID(WEAPON_TEMPERING_WORKBENCH_FORM_ID)
-    ));
+		// add reference to the workbench keyword
+		SetElementEditValues(recipeTemper, 'BNAM', GetEditValue(
+			getRecordByFormID(WEAPON_TEMPERING_WORKBENCH_FORM_ID)
+		));
 
-  end else if Signature(itemRecord) = 'ARMO' then begin
-    // set EditorID for recipe
-    SetElementEditValues(recipeTemper, 'EDID', 'TemperArmor' + GetElementEditValues(itemRecord, 'EDID'));
+	end else if Signature(itemRecord) = 'ARMO' then begin
+		// set EditorID for recipe
+		SetElementEditValues(recipeTemper, 'EDID', 'TemperArmor' + GetElementEditValues(itemRecord, 'EDID'));
 
-    // add reference to the workbench keyword
-    SetElementEditValues(recipeTemper, 'BNAM', GetEditValue(
-      getRecordByFormID(ARMOR_TEMPERING_WORKBENCH_FORM_ID)
-    ));
-  end;
+		// add reference to the workbench keyword
+		SetElementEditValues(recipeTemper, 'BNAM', GetEditValue(
+			getRecordByFormID(ARMOR_TEMPERING_WORKBENCH_FORM_ID)
+		));
+	end;
 
-  // figure out required component...
-  addItem(recipeItems, getMainMaterial(itemRecord), 1);
+	// figure out required component...
+	addItem(recipeItems, getMainMaterial(itemRecord), 1);
 
-  // remove nil record in items requirements, if any
-  removeInvalidEntries(recipeTemper);
+	// remove nil record in items requirements, if any
+	removeInvalidEntries(recipeTemper);
 
-  if GetElementEditValues(recipeTemper, 'COCT') = '' then begin
-    warn('no item requirements was specified for - ' + Name(recipeTemper));
-  end;
+	if GetElementEditValues(recipeTemper, 'COCT') = '' then begin
+		warn('no item requirements was specified for - ' + Name(recipeTemper));
+	end;
 
-  // return created tempering recipe, just in case
-  Result := recipeTemper;
+	// return created tempering recipe, just in case
+	Result := recipeTemper;
 end;
